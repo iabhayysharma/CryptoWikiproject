1e0a9d534c7b1f6df381f23dc4f7fb616c87ac5b
Abhay Sharma <abhays948@gmail.com>
2020-07-25 18:04:23 +0530

    Updates

---
+++ precache-manifest.0c49cb1c17c0094246a39bc397fda7b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24c2bdbfd0d10a5d6120cd62c1536083",
+    "url": "/CryptoWikiproject/index.html"
+  },
+  {
+    "revision": "52ed528013de7c5f3bee",
+    "url": "/CryptoWikiproject/static/css/2.b4d7a544.chunk.css"
+  },
+  {
+    "revision": "0773d4baaf1688153194",
+    "url": "/CryptoWikiproject/static/css/main.8412d25a.chunk.css"
+  },
+  {
+    "revision": "52ed528013de7c5f3bee",
+    "url": "/CryptoWikiproject/static/js/2.d60fa935.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/CryptoWikiproject/static/js/2.d60fa935.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0773d4baaf1688153194",
+    "url": "/CryptoWikiproject/static/js/main.6638213c.chunk.js"
+  },
+  {
+    "revision": "125a593791c75a35b40a",
+    "url": "/CryptoWikiproject/static/js/runtime-main.b58c3bae.js"
+  }
+]);
